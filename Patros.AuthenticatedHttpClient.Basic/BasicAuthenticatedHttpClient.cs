339db8c747de94aac5533aa6c670d234429bd5ac
Patrick Dwyer <pat@patdwyer.com.au>
2018-10-12 00:26:50 +1000

    Add link to RFC 1945 for basic auth

--- Patros.AuthenticatedHttpClient.Basic/BasicAuthenticatedHttpClient.cs
+++ Patros.AuthenticatedHttpClient.Basic/BasicAuthenticatedHttpClient.cs
@@ -20,6 +20,7 @@ namespace Patros.AuthenticatedHttpClient
 
         internal static string GenerateAuthenticationParameter(string userId, string password)
         {
+            // implemented as per RFC 1945 https://tools.ietf.org/html/rfc1945
             var userPass = string.Format("{0}:{1}", userId, password);
             var userPassBytes = System.Text.Encoding.UTF8.GetBytes(userPass);
             var userPassBase64 = System.Convert.ToBase64String(userPassBytes);
