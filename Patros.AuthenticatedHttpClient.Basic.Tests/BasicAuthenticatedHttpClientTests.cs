6134da553d8ab30c15280d8235aae18d240bb920
Patrick Dwyer <pat@patdwyer.com.au>
2018-10-13 19:46:56 +1000

    Update BasicAuthenticatedHttpClient with RFC 7617 details and fix missing Authorization header

--- Patros.AuthenticatedHttpClient.Basic.Tests/BasicAuthenticatedHttpClientTests.cs
+++ Patros.AuthenticatedHttpClient.Basic.Tests/BasicAuthenticatedHttpClientTests.cs
@@ -4,15 +4,24 @@ using Patros.AuthenticatedHttpClient;
 
 namespace Patros.AuthenticatedHttpClient.Basic.Tests
 {
-    // test values taken verbatim from RFC 1945
     public class BasicAuthenticatedHttpClientTests
     {
         [Fact]
-        public void TestGenerateAuthenticationParameter()
+        public void TestGenerateAuthenticationParameterRFC1945()
         {
+            // test value taken verbatim from RFC 1945
             var parameter = BasicAuthenticatedHttpMessageHandler.GenerateAuthenticationParameter("Aladdin", "OpenSesame");
 
             Assert.Equal("QWxhZGRpbjpPcGVuU2VzYW1l", parameter);
+        }
+
+        [Fact]
+        public void TestGenerateAuthenticationParameterRFC7617()
+        {
+            // test value taken verbatim from RFC 7617
+            var parameter = BasicAuthenticatedHttpMessageHandler.GenerateAuthenticationParameter("Aladdin", "open sesame");
+
+            Assert.Equal("QWxhZGRpbjpvcGVuIHNlc2FtZQ==", parameter);
         }
     }
 }
--- Patros.AuthenticatedHttpClient.Basic/BasicAuthenticatedHttpClient.cs
+++ Patros.AuthenticatedHttpClient.Basic/BasicAuthenticatedHttpClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Patros.AuthenticatedHttpClient
 {
@@ -22,11 +24,17 @@ namespace Patros.AuthenticatedHttpClient
 
         internal static string GenerateAuthenticationParameter(string userId, string password)
         {
-            // implemented as per RFC 1945 https://tools.ietf.org/html/rfc1945
+            // implemented as per RFC 7617 https://tools.ietf.org/html/rfc7617.html
             var userPass = string.Format("{0}:{1}", userId, password);
             var userPassBytes = System.Text.Encoding.UTF8.GetBytes(userPass);
             var userPassBase64 = System.Convert.ToBase64String(userPassBytes);
             return userPassBase64;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Authorization = _authorizationHeader;
+            return await base.SendAsync(request, cancellationToken);
         }
     }
 
