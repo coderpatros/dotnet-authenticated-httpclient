7898dd236ca50d75d04c60a4fb57c89a6213ea76
Patrick Dwyer <pat@patdwyer.com.au>
2018-11-07 22:25:27 +1000

    Add AuthorizationHeaderAuthenticatedHttpClient

---
+++ Patros.AuthenticatedHttpClient.AuthorizationHeader.Tests/AuthorizationHeaderAuthenticatedHttpClientTests.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xunit;
+using RichardSzalay.MockHttp;
+using Patros.AuthenticatedHttpClient;
+
+namespace Patros.AuthenticatedHttpClient.AuthorizationHeader.Tests
+{
+    public class AuthorizationHeaderAuthenticatedHttpClientTests
+    {
+        [Fact]
+        public async Task TestRequestAddsAuthenticationHeader()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp
+                .Expect("http://www.example.com")
+                .WithHeaders("Authorization", "test-value")
+                .Respond(HttpStatusCode.OK);
+            var client = AuthorizationHeaderAuthenticatedHttpClient.GetClient(new AuthorizationHeaderAuthenticatedHttpClientOptions
+            {
+                Value = "test-value"
+            }, mockHttp);
+
+            var responseContent = await client.GetStringAsync("http://www.example.com");
+
+            mockHttp.VerifyNoOutstandingExpectation();
+        }
+    }
+}
---
+++ Patros.AuthenticatedHttpClient.AuthorizationHeader/AuthorizationHeaderAuthenticatedHttpClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Patros.AuthenticatedHttpClient
+{
+    public class AuthorizationHeaderAuthenticatedHttpClientOptions
+    {
+        public string Value;
+    }
+
+    public class AuthorizationHeaderAuthenticatedHttpMessageHandler : DelegatingHandler
+    {
+        private AuthenticationHeaderValue _authorizationHeader;
+
+        public AuthorizationHeaderAuthenticatedHttpMessageHandler(AuthorizationHeaderAuthenticatedHttpClientOptions options, HttpMessageHandler innerHandler = null)
+        {
+            InnerHandler = innerHandler ?? new HttpClientHandler();
+            
+            _authorizationHeader = new AuthenticationHeaderValue(options.Value);
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Authorization = _authorizationHeader;
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+
+    public static class AuthorizationHeaderAuthenticatedHttpClient
+    {
+        public static HttpClient GetClient(AuthorizationHeaderAuthenticatedHttpClientOptions options, HttpMessageHandler innerHandler = null)
+        {
+            var msgHandler = new AuthorizationHeaderAuthenticatedHttpMessageHandler(options, innerHandler);
+            return new HttpClient(msgHandler);
+        }
+    }
+}
